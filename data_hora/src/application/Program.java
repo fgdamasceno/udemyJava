a7dde02ea8d9e8d29bbe4afe0c7f1f1e866581c4
fgdamasceno <fgdamasceno@gmail.com>
2024-10-19 19:49:21 -0300

    aulas data-hora

--- data_hora/src/application/Program.java
+++ data_hora/src/application/Program.java
@@ -1,11 +1,12 @@
 package application;
 
-import java.sql.Date;
+import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 public class Program {
 
@@ -16,7 +17,14 @@ public class Program {
 		 * https://docs.oracle.com/en/java/javase/20/docs/api/java.base/java/time/format/DateTimeFormatter.html
 		 * 
 		 * REGRAS PARA CONVERSÃO DE DATA-HORA PARA TEXTO E TEXTO PARA DATA-HORA
+		 * 
+		 * OBS.: 
+		 * IMPORTANTE => EM JAVA, DATA-HORA SÃO OBJETOS IMUTAVEIS. 
+		 * UMA VEZ INSTANCIADOS, NAO PODEM SER ALTERADOS.
+		 * PARA OBTER UMA HORA EDITADA, PEGA-SE UM OBJETO DATA-HORA, REALIZA-SE O CALCULO E O RESULTADO
+		 * É SALVO EM OUTRO OBJETO 
 		 */
+		
 
 		// ===> INSTANCIACAO <===
 
@@ -49,9 +57,10 @@ public class Program {
 		System.out.println("d10 = " + d10);
 		System.out.println("d11 = " + d11);
 
+		
 		// ---> DATA-HORA GLOBAL <---
 		Instant d03 = Instant.now(); // CAPTURA A DATA-HORA GLOBAL "GMT 0" (GREENWICH MERIDIONAL TIME)
-		Instant d06 = Instant.parse("2024-08-13T15:17:11Z"); // INSTANCIA O OBJETO COM DATA-HORA GLOBAL CUSTOMIZADA "GMT
+		Instant d06 = Instant.parse("2024-08-13T01:17:11Z"); // INSTANCIA O OBJETO COM DATA-HORA GLOBAL CUSTOMIZADA "GMT
 																// 0"
 		Instant d07 = Instant.parse("2024-08-13T15:17:11-03:00"); // INSTANCIA O OBJETO COM DATA-HORA GLOBAL CUSTOMIZADA
 																	// PARA "GMT-3)
@@ -59,6 +68,7 @@ public class Program {
 		System.out.println("d06 = " + d06);
 		System.out.println("d07 = " + d07);
 
+		
 		// ---> DATA-HORA PARA TEXTO
 		// ---> * DATA LOCAL
 		DateTimeFormatter fmt3 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
@@ -88,7 +98,67 @@ public class Program {
 		System.out.println("DATA-HORA GLOBAL PARA TEXTO = " + fmt7.format(d03));
 		
 		
+		// ---> CONVERTER DATA-HORA GLOBAL PARA LOCAL
+		// ZoneId.getAvailableZoneIds();
+		LocalDate conversao1 = LocalDate.ofInstant(d06, ZoneId.systemDefault());
+		System.out.println("CONVERSAO DATA-HORA GLOBAL PARA LOCAL = " + conversao1);
+		
+		LocalDate conversao2 = LocalDate.ofInstant(d06, ZoneId.of("Portugal"));
+		System.out.println("CONVERSAO DATA-HORA GLOBAL PARA LOCAL = " + conversao2);
+		
+		LocalDateTime conversao3 = LocalDateTime.ofInstant(d06, ZoneId.systemDefault());
+		System.out.println("CONVERSAO DATA-HORA GLOBAL PARA LOCAL = " + conversao3);
+		
+		LocalDateTime conversao4 = LocalDateTime.ofInstant(d06, ZoneId.of("Portugal"));
+		System.out.println("CONVERSAO DATA-HORA GLOBAL PARA LOCAL = " + conversao4);
+		
+		
+		// ---> OBTENDO DADOS DATA-HORA LOCAL
+		// LocalDate
+		System.out.println("DIA = " + d01.getDayOfMonth());
+		System.out.println("MES = " + d01.getMonthValue());
+		System.out.println("ANO = " + d01.getYear());
+		// LocalDateTime
+		System.out.println("HORA:MINUTO = " + d02.getHour() + ":" + d02.getMinute());
+		
+		
+		// ---> CALCULOS COM DATA E HORA
+		LocalDate pastWeekLocalDate = d01.minusDays(7);
+		System.out.println("SEMANA ATUAL = " + d01 + ", SEMANA ANTERIOR = " + pastWeekLocalDate);
+		
+		LocalDate nextWeekLocalDate = d01.plusDays(7);
+		System.out.println("SEMANA ATUAL = " + d01 + ", PROXIMA SEMANA = " + nextWeekLocalDate);
+		
+		LocalDateTime pastYearLocalDateTime = d02.minusYears(1);
+		System.out.println("ANO ATUAL = " + d02 + ", ANO ANTERIOR = " + pastYearLocalDateTime);
+		
+		LocalDateTime nextYearLocalDateTime = d02.plusYears(1);
+		System.out.println("ANO ATUAL = " + d02 + ", ANO ANTERIOR = " + nextYearLocalDateTime);
+		 
+		Instant pastDaysInstant = d03.minus(6, ChronoUnit.DAYS); // NAO SUPORT .MONTHS
+		System.out.println("DIA ATUAL = " + d03 + ", DIAS ANTERIORES = " + pastDaysInstant);
+		
+		Instant nextDaysInstant = d03.plus(6, ChronoUnit.DAYS); // NAO SUPORT .MONTHS
+		System.out.println("DIA ATUAL = " + d03 + ", PROXIMOS DIAS = " + nextDaysInstant);
+		
+		/*
+		 * UTILIZAM-SE OS MESMOS MÉTODOS PARA LocalDate, LocalDateTime e Instant
+		 */
+		
+		
 		// ---> DURATION <---
+		// LocalDate
+		// Duration d1 = Duration.between(pastWeekLocalDate.atTime(0, 0), d01.atTime(0, 0)); // ESTE E O DE BAIXO EXIBEM O MESMO RESULTADO
+		Duration d1 = Duration.between(pastWeekLocalDate.atStartOfDay(), d01.atStartOfDay());
+		System.out.println("DURAÇÃO = " + d1.toDays() + " DIAS");;		
+		
+		// LocalDateTime
+		Duration d2 = Duration.between(pastYearLocalDateTime, d02);
+		System.out.println("DURAÇÃO = " + d2.toDays() + " DIAS");
+		
+		// Instant
+		Duration d3 = Duration.between(pastDaysInstant, d03);
+		System.out.println("DURAÇÃO = " + d3.toDays() + " DIAS");
 
 		// ---> OUTROS <---
 
@@ -96,8 +166,8 @@ public class Program {
 		 * TODAS AS HORAS ACIMAS ESTÃO SENDO INSTANCIADAS COMO OBJETOS QUANDO VAMOS
 		 * IMPRIMÍ-LAS COM System.out, O OBJETO CHAMA SEU MÉTODO toString(); DESTA
 		 * FORMA, PODERIAMOS IGUALMENTE UTILIZAR A FORMA ABAIXO PARA EXIBÍ-LOS: EXEMPLO
-		 * -> System.out.println("d01 = " + d01.toString()); E TERIAMAS O MESMO
-		 * RESULTADO ESTE toString() FORMATA O TEXTO EXIBIDO NO FORMATO "ISO 8601" POR
+		 * -> System.out.println("d01 = " + d01.toString()); E TERIAMOS O MESMO
+		 * RESULTADO. ESTE toString() FORMATA O TEXTO EXIBIDO-O NO FORMATO "ISO 8601" POR
 		 * PADRÃO
 		 * 
 		 * NA FORMATAÇÃO UTILIZANDO DateTimeFormatter, A DATA PASSADA COMO ARGUMENTO
