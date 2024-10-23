4561cb24374e32407ff96a79a38ca5b5138f8a1c
fgdamasceno <fgdamasceno@gmail.com>
2024-10-23 15:12:51 -0300

    exercicio resolvido composicao

---
+++ enumeracao_composicao/src/entities/Department.java
@@ -0,0 +1,23 @@
+package entities;
+
+//PARTE DO EXERCICIO RESOLVIDO 1
+public class Department {
+	
+	private String name;
+
+	public Department() {
+	}
+
+	public Department(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
---
+++ enumeracao_composicao/src/entities/enums/WorkerLevel.java
@@ -0,0 +1,10 @@
+package entities.enums;
+
+// PARTE DO EXERCICIO RESOLVIDO 1
+public enum WorkerLevel {	
+
+	JUNIOR,
+	MID_LEVEL,
+	SENIOR;
+	
+}
