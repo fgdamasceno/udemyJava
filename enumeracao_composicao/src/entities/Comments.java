583b81933aad4f90a8ef804dd3d39444f7dd9918
fgdamasceno <fgdamasceno@gmail.com>
2024-10-23 16:40:11 -0300

    exercicios composição e StringBuilder

---
+++ enumeracao_composicao/src/entities/Comments.java
@@ -0,0 +1,23 @@
+package entities;
+
+// PARTE DO EXERCICIO RESOLVIDO 2
+public class Comments {
+
+	private String text;
+
+	public Comments() {
+	}
+
+	public Comments(String text) {
+		this.text = text;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+}
