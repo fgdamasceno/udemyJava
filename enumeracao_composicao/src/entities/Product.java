a8972095b311d45f5bfbef5f17f068d07722bf6b
fgdamasceno <fgdamasceno@gmail.com>
2024-10-24 20:27:00 -0300

    exercicio fixacao composicao

---
+++ enumeracao_composicao/src/entities/Product.java
@@ -0,0 +1,33 @@
+package entities;
+
+// PARTE DO EXERCICIO DE FIXACAO
+public class Product {
+	
+	private String name;
+	private Double price;
+	
+	public Product() {
+	}
+
+	public Product(String name, double price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+}
---
+++ enumeracao_composicao/src/entities/enums/OrderStatusExFix.java
@@ -0,0 +1,11 @@
+package entities.enums;
+
+// PARTE DO EXERCICIO DE FIXACAO
+public enum OrderStatusExFix {
+	
+	PENDING_PAYMENT,
+	PROCESSING,
+	SHIPPED,
+	DELIVERED;
+
+}
