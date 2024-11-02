7c26104b5b0de033d7705c30a6fe93de26ade7ba
fgdamasceno <fgdamasceno@gmail.com>
2024-11-02 13:13:54 -0300

    solucao muito ruim

--- tratamento_excecoes/src/problema_exemplo/application/Program.java
+++ tratamento_excecoes/src/problema_exemplo/application/Program.java
@@ -37,6 +37,30 @@ public class Program {
 			System.out.print("Check-out date (dd/MM/yyyy): ");
 			checkOut = sdf.parse(sc.next());
 			
+			// SOLUÇÃO MUITO RUIM
+			/*
+			
+			Date now = new Date();
+			if (checkIn.before(now) || checkOut.before(now)) {
+				System.out.println("Error in reservation: Reservation dates for update must be future dates.");
+			}
+			else if (!checkOut.after(checkIn)) {
+				System.out.println("Error in reservation: Check-out date must be after check-in date.");
+			}
+			else {
+				reservation.updateDates(checkIn, checkOut);
+				System.out.println("Reservation: " + reservation);	
+			}
+			
+			*/
+			// FIM SOLUÇÃO MUITO RUIM
+			// ESTA SOLUÇÃO DEVERIA ESTAR NA CLASSE 'RESERVATION', RESPONSAVEL PELA RESERVA, (delegação) NAO NO PROGRAMA PRINCIPAL
+			
+			// =========================================================================================
+			
+			// SOLUÇÃO RUIM
+			
+			
 			reservation.updateDates(checkIn, checkOut);
 			System.out.println("Reservation: " + reservation);			
 		}
