07fac5155685b06f7351963f42555099a23416e2
fgdamasceno <fgdamasceno@gmail.com>
2024-11-05 17:21:48 -0300

    manipulando pastas

--- manipulando_pastas_com_file/src/application/Program.java
+++ manipulando_pastas_com_file/src/application/Program.java
@@ -10,25 +10,41 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 
 		System.out.println("Enter a folder path: ");
-		String strPath = sc.nextLine();
+		String folderPath = sc.nextLine();
 		
-		File path = new File(strPath); // File: CAMINHO DE ARQUIVO OU PASTA
+		File foPath = new File(folderPath); // File: CAMINHO DE ARQUIVO OU PASTA
 		
-		File[] folders = path.listFiles(File::isDirectory); // CRIA UM VETOR DE ARQUIVOS CONTENDO O CAMINHO PARA CADA PASTA NO CAMINHO ESPECIFICADO
+		File[] folders = foPath.listFiles(File::isDirectory); // CRIA UM VETOR DE ARQUIVOS CONTENDO O CAMINHO PARA CADA PASTA NO CAMINHO ESPECIFICADO
 		System.out.println("FOLDERS:");
 		for (File folder : folders) {
 			System.out.println(folder);
 		}
 		
-		File[] files = path.listFiles(File::isFile); // CRIA UM VETOR DE ARQUIVOS CONTENTO O CAMINHO PARA CADA ARQUIVO NO CAMINHO ESPECIFICADO
+		File[] files = foPath.listFiles(File::isFile); // CRIA UM VETOR DE ARQUIVOS CONTENTO O CAMINHO PARA CADA ARQUIVO NO CAMINHO ESPECIFICADO
 		System.out.println("FILES:");
 		for (File file: files) {
 			System.out.println(file);
 		}
 		
 		// CRIANDO UMA PASTA EM UM DIRETÓRIO ESPECIFICO
-		boolean success = new File(strPath + "\\folder4").mkdir();
+		boolean success = new File(foPath + "\\folder4").mkdir();
 		System.out.println("Directory created successfully! " + success);
+		
+		
+		// INFORMAÇÕES DO CAMINHO DO ARQUIVOS
+		System.out.println("Enter a file path:");
+		String filePath = sc.nextLine();
+		
+		File fiPath = new File(filePath);
+		
+		// ACESSAR SOMENTE O NOME DO ARQUIVO, E NÃO SEU CAMINHO COMPLETO
+		System.out.println("getName: " + fiPath.getName()); // get.Name() RETORNA SOMENTE O NOME DO ARQUIVO
+		System.out.println("getName: " + fiPath.getPath()); // get.Path() RETORNA O CAMINHO COMPLETO PARA O ARQUIVO
+		System.out.println("getName: " + fiPath.getParent()); // get.Path() RETORNA O CAMINHO COMPLETO PARA O ARQUIVO
+		
+		
+		
+		
 		
 		sc.close();
 	}
